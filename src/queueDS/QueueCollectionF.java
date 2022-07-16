78595fad77a98fa9b464227997f4a98f4961b7a2
pravinvns789 <pravin.singh_cs19@gla.ac.in>
2022-07-17 04:21:33 +0530

    queueLinkedList

---
+++ src/queueDS/QueueCollectionF.java
@@ -0,0 +1,20 @@
+package queueDS;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueCollectionF {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> qu = new ArrayDeque<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println("removed "+ q.remove());
+        while (!q.isEmpty()){
+            System.out.println(q.remove());
+        }
+
+    }
+}
