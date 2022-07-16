4d4c522823bf59af62600df05b4d7783231f6315
pravinvns789 <pravin.singh_cs19@gla.ac.in>
2022-07-17 04:15:35 +0530

    queueLinkedList

--- src/queueDS/QueueLinkedList.java
+++ src/queueDS/QueueLinkedList.java
@@ -1,4 +1,54 @@
 package queueDS;
 
 public class QueueLinkedList {
+    Node front=null;
+    Node rear=null;
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            next=null;
+        }
+    }
+    public boolean isEmpty(){
+        return front==null && rear==null;
+    }
+    //enqueue
+    public void add(int data){
+        Node newNode = new Node(data);
+        if(front==null && rear==null){
+            front=rear=newNode;
+        }
+        rear.next=newNode;
+        rear=newNode;
+    }
+    public int remove(){
+        if(isEmpty()){
+            return -1;
+        }
+        if(front==rear){
+            rear=null;
+        }
+        int val = front.data;
+        front=front.next;
+        return val;
+    }
+    public int peek(){
+        if (isEmpty()){
+            return -1;
+        }
+        return front.data;
+    }
+
+    public static void main(String[] args) {
+        QueueLinkedList q = new QueueLinkedList();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println("removed "+ q.remove());
+        while (!q.isEmpty()){
+            System.out.println(q.remove());
+        }
+    }
 }
